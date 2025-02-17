33bd4c1a31a26018586494c85c99c5e320d7e2fb
uday <uday.bytes@gmail.com>
2025-02-17 10:18:32 +0530

    Async performance improvement for menu loadings

--- AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Middleware/StatisticMiddleware.cs
@@ -21,22 +21,23 @@ public class StatisticMiddleware
 
     public async Task InvokeAsync(HttpContext context)
     {
+
         string path = context.Request.Path;
 
-        var staticRegTask = Task.Run(
-            () => _statisticService.RegisterVisitAsync(path)
-                .ConfigureAwait(false)
-                .GetAwaiter().OnCompleted(UpdateHeaders));
+        Task staticRegTask = Task.Run(
+          () => _statisticService.RegisterVisitAsync(path).ConfigureAwait(false));
+        await staticRegTask;
         Console.WriteLine(staticRegTask.Status); // just for debugging purposes
 
-        void UpdateHeaders()
+        //Update Headers
+        await Task.Run(async () =>
         {
+            var res = await _statisticService.GetVisitsCountAsync(path);
+
             context.Response.Headers.Add(
                 CustomHttpHeaders.TotalPageVisits,
-                _statisticService.GetVisitsCountAsync(path).GetAwaiter().GetResult().ToString());
-        }
-
-        Thread.Sleep(3000); // without this the statistic counter does not work
+                res.ToString());
+        });
         await _next(context);
     }
 }
--- AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
 using CloudServices.Interfaces;
 
@@ -20,8 +19,9 @@ public class ManualAssistant : IAssistant
         try
         {
             var t = _supportService.RegisterSupportRequestAsync(requestInfo);
+            await t;
             Console.WriteLine(t.Status); // this is for debugging purposes
-            Thread.Sleep(5000); // this is just to be sure that the request is registered
+            //Thread.Sleep(5000); // this is just to be sure that the request is registered
             return await _supportService.GetSupportInfoAsync(requestInfo)
                 .ConfigureAwait(false);
         }
